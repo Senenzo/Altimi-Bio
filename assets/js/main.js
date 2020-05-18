6da761df1d4bfb07abdfea449d9ac2a955e30c4c
Meddis <mahadasim@hotmail.com>
2020-05-18 20:02:49 +0500

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,13 +198,3 @@
 
 })(jQuery);
 
-$('.button').click(function(){
-  var buttonId = $(this).attr('id');
-  $('#modal-container').removeAttr('class').addClass(buttonId);
-  $('body').addClass('modal-active');
-})
-
-$('#modal-container').click(function(){
-  $(this).addClass('out');
-  $('body').removeClass('modal-active');
-});
